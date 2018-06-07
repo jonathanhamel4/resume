67ae0ce07f5ac16d93f27b6225b956c1d17167cc
jhamel <jhamel@vigilantglobal.com>
2018-06-07 17:37:52 -0400

    some basic image layering template

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, Inject } from '@angular/core';
 import { trigger, state, style, animate, transition, group } from '@angular/animations';
+import { DOCUMENT } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-header',
@@ -14,7 +15,21 @@ import { trigger, state, style, animate, transition, group } from '@angular/anim
   ]
 })
 export class HeaderComponent {
-  state = 'closed';
+  public scrollDarkBackground = false;
+  public state = 'closed';
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const number = window.pageYOffset || this.document.documentElement.scrollTop || this.document.body.scrollTop || 0;
+    console.log(number);
+    if (number > 930) {
+      this.scrollDarkBackground = true;
+    } else if (this.scrollDarkBackground && number < 10) {
+      this.scrollDarkBackground = false;
+    }
+  }
+
+  constructor(@Inject(DOCUMENT) private document: Document) { }
 
   public toggleCollapse() {
     this.state = this.state === 'closed' ? 'open' : 'closed';
