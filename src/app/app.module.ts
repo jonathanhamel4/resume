32e6829b84f585c9eb562003308510b1dbaa3269
jhamel <jhamel@vigilantglobal.com>
2018-06-11 12:07:41 -0400

    use VMWare Clarity & base persona section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { ClarityModule } from '@clr/angular';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { LayoutComponent } from './layout/layout.component';
+import { PersonaComponent } from './persona/persona.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    LayoutComponent,
+    PersonaComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ClarityModule
   ],
   providers: [],
   bootstrap: [AppComponent]
