41fa8d92dc4e8414e4a17fc53584e4d934913528
jhamel <jhamel@vigilantglobal.com>
2018-07-04 14:44:52 -0400

    video homepage

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { Link } from '../../models/link';
 import { LinkService } from '../../services/link.service';
 import { TranslateService } from '@ngx-translate/core';
+import Typed from 'typed.js';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.css']
 })
-export class LayoutComponent implements OnInit {
+export class LayoutComponent implements OnInit, AfterViewInit {
 
   public links: Link[] = [];
   public currentLang = '';
@@ -18,6 +19,25 @@ export class LayoutComponent implements OnInit {
   public ngOnInit() {
     this.currentLang = this.translate.currentLang;
     this.links = this.linkService.sectionLinks;
+  }
+
+  public ngAfterViewInit() {
+    new Typed(".typewriter", { 
+      strings: [
+        "Hi, my name is Jonathan",
+        "I'm a Web Developer", 
+        "I'm an Engineer",
+        "I'm a Software Engineer",
+        "Here is my interactive resume"],
+      typeSpeed: 50,
+      startDelay: 500,
+      smartBackspace: true,
+      backSpeed: 20,
+      loop: false,
+      showCursor: true,
+      cursorChar: "|",
+      autoInsertCss: true
+    });
   }
 
   public setCurrentLang() {
@@ -30,4 +50,10 @@ export class LayoutComponent implements OnInit {
     }
   }
 
+  public scrollToAbout() {
+    document.querySelector('#about').scrollIntoView({ 
+      behavior: 'smooth',
+      block: "start"
+    });
+  }
 }
