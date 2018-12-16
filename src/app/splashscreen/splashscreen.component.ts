b570dd0fca60e7339c65adc761bfea9e00ea1ea5
Jonathan Hamel <jonathanhamel4@gmail.com>
2018-12-15 20:12:04 -0500

    modify i18n and splashscreen

--- src/app/splashscreen/splashscreen.component.ts
+++ src/app/splashscreen/splashscreen.component.ts
@@ -1,30 +1,40 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit, OnInit } from '@angular/core';
 import Typed from 'typed.js';
 import { TranslateService } from '@ngx-translate/core';
-
+import { trigger, state, style, transition, animate } from '@angular/animations';
+import { Utils } from '../../services/Utils';
 
 @Component({
   selector: 'app-splashscreen',
   templateUrl: './splashscreen.component.html',
   styleUrls: ['./splashscreen.component.css']
 })
-export class SplashscreenComponent implements AfterViewInit {
+export class SplashscreenComponent implements AfterViewInit, OnInit {
 
   private typed: any;
 
+  public header: string = "";
+  public delayTitle: string = "";
+  public delayArrow: string = "";
+
   constructor(private translate: TranslateService) { }
 
+  public ngOnInit() {
+    this.appearDelay("Jonathan Hamel", 2000, "header");
+    Utils.setValueDelay("ready", ["delayTitle", "delayArrow"], this, 3000);
+  }
+
   public ngAfterViewInit() {
-    this.initiateTypeWriter();
+    // this.initiateTypeWriter();
 
-    this.translate.onLangChange.subscribe(() => {
-      this.initiateTypeWriter();
-    });
+    // this.translate.onLangChange.subscribe(() => {
+    //   this.initiateTypeWriter();
+    // });
 
-    const video = document.getElementById('videoHomepage') as HTMLVideoElement;
-    video.addEventListener('click', () => {
-      video.play();
-    }, false);
+    // const video = document.getElementById('videoHomepage') as HTMLVideoElement;
+    // video.addEventListener('click', () => {
+    //   video.play();
+    // }, false);
   }
 
   private initiateTypeWriter() {
@@ -55,4 +65,17 @@ export class SplashscreenComponent implements AfterViewInit {
     });
   }
 
+  private appearDelay(msg: string, delay: number, varReference: string, isFirstLetter=true) {
+    if(isFirstLetter) {
+      this[varReference] = "";
+    }
+
+    if(msg) {
+      const timeout = setTimeout(() => {
+        this[varReference] += msg[0];
+        this.appearDelay(msg.substring(1), delay, varReference, false);
+        clearTimeout(timeout);
+      }, (isFirstLetter ? delay : 25));
+    }
+  }
 }
---
+++ src/services/Utils.ts
@@ -0,0 +1,16 @@
+import { Component } from "@angular/core";
+
+export class Utils {
+    public static setValueDelay(value: any, varReference: string|string[], container: any, delay: number) {
+        const timeout = setTimeout(() => {
+            clearTimeout(timeout);
+            if(Array.isArray(varReference)) {
+                for(const reference of varReference) {
+                    container[reference] = value;
+                }
+            } else {
+                container[varReference] = value;
+            }
+        },  delay);
+    }
+}
