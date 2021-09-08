94a46bffb383e25eb370281ed375b9b7dba528a4
jonathanhamel4 <jonathanhamel4@gmail.com>
2021-09-07 21:15:18 -0400

    add work trails

--- src/app/work/work.component.ts
+++ src/app/work/work.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { TranslateService } from "@ngx-translate/core";
 
 @Component({
-  selector: 'app-work',
-  templateUrl: './work.component.html',
-  styleUrls: ['./work.component.css']
+  selector: "app-work",
+  templateUrl: "./work.component.html",
+  styleUrls: ["./work.component.css"],
 })
 export class WorkComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private translate: TranslateService) {
+    translate.setDefaultLang("en");
   }
 
+  ngOnInit() {}
+
+  public getWork() {
+    const work = this.translate.instant("WORK");
+    return Array.isArray(work) ? work : [];
+  }
+
+  public getWorkClass(i: number) {
+    const work = this.getWork();
+    const previousSame = work[i - 1] && work[i - 1].COMPANY === work[i].COMPANY;
+    const nextSame = work[i + 1] && work[i + 1].COMPANY === work[i].COMPANY;
+
+    if (previousSame && nextSame) {
+      return "dot-up dot-down";
+    }
+
+    if (previousSame) {
+      return "dot-up";
+    }
+
+    if (nextSame) {
+      return "dot-down";
+    }
+  }
 }
