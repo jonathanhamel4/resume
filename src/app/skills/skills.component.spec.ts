624424eff119faba5fee3944184601ec4f85671a
jonathanhamel4 <jonathanhamel4@gmail.com>
2021-09-07 20:20:58 -0400

    update skills section

--- src/app/skills/skills.component.spec.ts
+++ src/app/skills/skills.component.spec.ts
@@ -1,49 +1,50 @@
-import { async, ComponentFixture, TestBed, getTestBed } from '@angular/core/testing';
-import { SkillsComponent } from './skills.component';
-import { TranslateModule, TranslateLoader, TranslateService } from '@ngx-translate/core';
-import { AnimatedProgressComponent } from '../animated-progress/animated-progress.component';
-import { ClarityModule } from '@clr/angular';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { Observable } from 'rxjs/Observable';
-import { MockHttpLoader } from '../../services/mockTranslateLoader.service';
-import 'rxjs/add/observable/of';
+import {
+  async,
+  ComponentFixture,
+  TestBed,
+  getTestBed,
+} from "@angular/core/testing";
+import { SkillsComponent } from "./skills.component";
+import {
+  TranslateModule,
+  TranslateLoader,
+  TranslateService,
+} from "@ngx-translate/core";
+import { AnimatedProgressComponent } from "../animated-progress/animated-progress.component";
+import { ClarityModule } from "@clr/angular";
+import { MockHttpLoader } from "../../services/mockTranslateLoader.service";
+import "rxjs/add/observable/of";
 
-describe('Skills Component', () => {
-    let component: SkillsComponent;
-    let fixture: ComponentFixture<SkillsComponent>;
-    let translate: TranslateService;
+describe("Skills Component", () => {
+  let component: SkillsComponent;
+  let fixture: ComponentFixture<SkillsComponent>;
+  let translate: TranslateService;
 
-    beforeEach(() => {
-        TestBed.configureTestingModule({
-            imports: [
-                TranslateModule.forRoot({
-                    loader: {
-                        provide: TranslateLoader,
-                        useClass: MockHttpLoader
-                    }
-                }),
-                ClarityModule],
-            declarations: [SkillsComponent, AnimatedProgressComponent],
-            providers: []
-        }).compileComponents();
-    });
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useClass: MockHttpLoader,
+          },
+        }),
+        ClarityModule,
+      ],
+      declarations: [SkillsComponent, AnimatedProgressComponent],
+      providers: [],
+    }).compileComponents();
+  });
 
-    beforeEach(() => {
-        fixture = TestBed.createComponent(SkillsComponent);
-        component = fixture.componentInstance;
-        translate = getTestBed().get(TranslateService);
-        translate.use('en');
-        fixture.detectChanges();
-    });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SkillsComponent);
+    component = fixture.componentInstance;
+    translate = getTestBed().get(TranslateService);
+    translate.use("en");
+    fixture.detectChanges();
+  });
 
-    it('should be created', () => {
-        expect(component).toBeTruthy();
-    });
-
-    it('should translate skills', () => {
-        const elements = fixture.nativeElement.querySelectorAll('h5');
-        expect(elements.length).toBe(2);
-        expect(elements[0].innerHTML).toEqual('Soft Skills');
-        expect(elements[1].innerHTML).toEqual('Hard Skills');
-    });
+  it("should be created", () => {
+    expect(component).toBeTruthy();
+  });
 });
--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,65 +1,41 @@
-import { Component, HostListener, ElementRef } from '@angular/core';
-import { Skill } from '../../models/skill';
+import { Component, HostListener, ElementRef } from "@angular/core";
+import { Skill } from "../../models/skill";
 
 @Component({
-  selector: 'app-skills',
-  templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.css']
+  selector: "app-skills",
+  templateUrl: "./skills.component.html",
+  styleUrls: ["./skills.component.css"],
 })
 export class SkillsComponent {
-
   public showLoaders = false;
 
-  public softSkills: Skill[] = [
-    { key: 'Leadership', currentValue: 0, value: 60 } as Skill,
-    { key: 'Work Ethic', currentValue: 0, value: 85 } as Skill,
-    { key: 'Positive Attitude', currentValue: 0, value: 80 } as Skill,
-    { key: 'Communication', currentValue: 0, value: 90, tooltip: 'English & French', tooltipClass: 'tooltip-sm' } as Skill,
-    { key: 'Time Management', currentValue: 0, value: 70 } as Skill,
-    { key: 'Problem-Solving Skills', currentValue: 0, value: 80 } as Skill,
-    { key: 'Team Player', currentValue: 0, value: 95 } as Skill,
-    { key: 'Confidence', currentValue: 0, value: 80 } as Skill,
-    { key: 'Learn from Criticism', currentValue: 0, value: 70 } as Skill,
-    { key: 'Work under pressure', currentValue: 0, value: 80 } as Skill,
-    { key: 'Adaptability', currentValue: 0, value: 85 } as Skill
-  ];
-
   public hardSkills: Skill[] = [
-    { key: 'Java', currentValue: 0, value: 50 } as Skill,
-    { key: 'Javascript', currentValue: 0, value: 95 } as Skill,
-    { key: '.NET & .NET Core', currentValue: 0, value: 80 } as Skill,
-    { key: 'AngularJs', currentValue: 0, value: 70 } as Skill,
-    { key: 'Angular 2+', currentValue: 0, value: 80 } as Skill,
-    { key: 'ReactJs', currentValue: 0, value: 80 } as Skill,
-    { key: 'Nodejs + modules', currentValue: 0, value: 90 } as Skill,
-    { key: 'Python', currentValue: 0, value: 85 } as Skill,
-    { key: 'VueJs', currentValue: 0, value: 15 } as Skill,
-    { key: 'Ruby', currentValue: 0, value: 10 } as Skill,
-    { key: 'Ansible', currentValue: 0, value: 70 } as Skill,
-    { key: 'Capistrano', currentValue: 0, value: 70 } as Skill,
-    { key: 'Shell & Bash', currentValue: 0, value: 40 } as Skill,
-    { key: 'PHP', currentValue: 0, value: 40 } as Skill,
-    { key: 'SQL', currentValue: 0, value: 60 } as Skill
+    { key: "Java", currentValue: 0, value: 50 } as Skill,
+    { key: "JavaScript/Node.js", currentValue: 0, value: 95 } as Skill,
+    { key: ".NET & .NET Core", currentValue: 0, value: 70 } as Skill,
+    { key: "AngularJs", currentValue: 0, value: 60 } as Skill,
+    { key: "Angular 2+", currentValue: 0, value: 80 } as Skill,
+    { key: "ReactJs", currentValue: 0, value: 90 } as Skill,
+    { key: "Python", currentValue: 0, value: 85 } as Skill,
+    { key: "Ruby", currentValue: 0, value: 40 } as Skill,
+    { key: "Ansible", currentValue: 0, value: 70 } as Skill,
+    { key: "Bash", currentValue: 0, value: 50 } as Skill,
+    { key: "SQL", currentValue: 0, value: 60 } as Skill,
+    { key: "Golang", currentValue: 0, value: 40 } as Skill,
   ];
 
-  public softSkillsExpanded = false;
   public hardSkillsExpanded = false;
 
   constructor(private el: ElementRef) {
-    this.softSkills = this.softSkills.sort((a: Skill, b: Skill) => a.key.localeCompare(b.key));
-    this.hardSkills = this.hardSkills.sort((a: Skill, b: Skill) => a.key.localeCompare(b.key));
-  }
-
-  public getSoftSkills(): Skill[] {
-    return this.softSkillsExpanded ? this.softSkills : this.softSkills.slice(0, 10);
+    this.hardSkills = this.hardSkills.sort(
+      (a: Skill, b: Skill) => b.value - a.value
+    );
   }
 
   public getHardSkills(): Skill[] {
-    return this.hardSkillsExpanded ? this.hardSkills : this.hardSkills.slice(0, 10);
-  }
-
-  public toggleSoftSkillsExpanded(): void {
-    this.softSkillsExpanded = !this.softSkillsExpanded;
+    return this.hardSkillsExpanded
+      ? this.hardSkills
+      : this.hardSkills.slice(0, 7);
   }
 
   public toggleHardSkillsExpanded(): void {
