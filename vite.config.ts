b993a56adcbcd4d62dafb5769c4e1ecd978e2622
jonathanhamel4 <jonathanhamel4@gmail.com>
2024-07-18 08:43:44 -0400

    add autoprefixer

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import autoprefixer from "autoprefixer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // @ts-ignore
+  css: {
+    postcss: {
+      plugins: [autoprefixer()],
+    },
+  },
   base: "/resume/",
 });
